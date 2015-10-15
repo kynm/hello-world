ec2618a0fc46ca2f66c0887010ef4304fb39bfa5
kynm <nguyen.manh.ky@framgia.com>
2015-10-15 14:51:28 +0700

    xxxx

--- src/SayHello.php
+++ src/SayHello.php
@@ -1,11 +1,17 @@
 <?php 
 
-namespace kynm\SayHello;
+namespace SayHello;
 
 class SayHello
 {
     public static function world()
     {
         return 'Hello World, Composer!';
+    }
+
+    static function area($side){
+      $surface_area = $side * $side;
+
+      echo "The squares surface area is $surface_area units\n";
     }
 }
